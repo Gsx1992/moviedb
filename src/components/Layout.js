a7402eb49426ca15b71f5d5bd9d1ea52942a9d9a
Gareth Lacey <gareth@Gareths-MacBook-Pro.local>
2021-01-18 16:14:36 +0800

    refactored some ui layout components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,35 +1,20 @@
 import React from "react";
-import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
-import StarIcon from "@material-ui/icons/StarBorder";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
 import { makeStyles, createMuiTheme } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
 import { ThemeProvider } from "@material-ui/styles";
 import CardMedia from "@material-ui/core/CardMedia";
-import CardActionArea from '@material-ui/core/CardActionArea';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import CardActionArea from "@material-ui/core/CardActionArea";
+import Header from "./Header";
+import Footer from "./Footer";
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@material-ui/core";
 
 const theme = createMuiTheme({
   palette: {
@@ -48,6 +33,19 @@ const theme = createMuiTheme({
   },
 });
 
+const items = [
+  {
+    name: "Movie title 1",
+    description: "Probably the most random thing you have ever seen!",
+    releaseDate: "25/06/2001"
+  },
+  {
+    name: "Movie title 2",
+    description: "Hello mate",
+    releaseDate: "25/06/2001"
+  },
+];
+
 const useStyles = makeStyles((theme) => ({
   "@global": {
     ul: {
@@ -55,15 +53,6 @@ const useStyles = makeStyles((theme) => ({
       padding: 0,
       listStyle: "none",
     },
-  },
-  appBar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-  toolbar: {
-    flexWrap: "wrap",
-  },
-  toolbarTitle: {
-    flexGrow: 1,
   },
   link: {
     margin: theme.spacing(1, 1.5),
@@ -77,98 +66,27 @@ const useStyles = makeStyles((theme) => ({
         ? theme.palette.grey[200]
         : theme.palette.grey[700],
   },
-  cardPricing: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "baseline",
-    marginBottom: theme.spacing(2),
-  },
-  footer: {
-    borderTop: `1px solid ${theme.palette.divider}`,
-    marginTop: theme.spacing(8),
-    paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(3),
-    [theme.breakpoints.up("sm")]: {
-      paddingTop: theme.spacing(6),
-      paddingBottom: theme.spacing(6),
-    },
-    root: {
-        maxWidth: 345,
-      },
-      media: {
-        height: 140,
-      },
-  },
 }));
 
-const tiers = [
-  {
-    title: "Free",
-    price: "0",
-    description: [
-      "10 users included",
-      "2 GB of storage",
-      "Help center access",
-      "Email support",
-    ],
-    buttonText: "Sign up for free",
-    buttonVariant: "outlined",
-  },
-  {
-    title: "Pro",
-    subheader: "Most popular",
-    price: "15",
-    description: [
-      "20 users included",
-      "10 GB of storage",
-      "Help center access",
-      "Priority email support",
-    ],
-    buttonText: "Get started",
-    buttonVariant: "contained",
-  },
-  {
-    title: "Enterprise",
-    price: "30",
-    description: [
-      "50 users included",
-      "30 GB of storage",
-      "Help center access",
-      "Phone & email support",
-    ],
-    buttonText: "Contact us",
-    buttonVariant: "outlined",
-  },
-];
-const footers = [
-  {
-    title: "Company",
-    description: ["Team", "History", "Contact us", "Locations"],
-  },
-  {
-    title: "Features",
-    description: [
-      "Cool stuff",
-      "Random feature",
-      "Team feature",
-      "Developer stuff",
-      "Another one",
-    ],
-  },
-  {
-    title: "Resources",
-    description: [
-      "Resource",
-      "Resource name",
-      "Another resource",
-      "Final resource",
-    ],
-  },
-  {
-    title: "Legal",
-    description: ["Privacy policy", "Terms of use"],
-  },
-];
+function Item(props) {
+  return (
+    <Paper>
+      <Grid container spacing={5}>
+        <Grid item xs={4} style = {{paddingBottom: '0px'}}>
+          <Paper circle={true} style={{overflow:'hidden'}}>
+          <img src='placeholder.jpg' style={{width:'100%', height:'100%'}}/>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={6}>
+          <h2>{props.item.name}</h2>
+          <p>{props.item.description}</p>
+          <p>Release date: {props.item.releaseDate}</p>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
 
 export default function Layout() {
   const classes = useStyles();
@@ -177,52 +95,7 @@ export default function Layout() {
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <AppBar position="static" elevation={0} className={classes.appBar}>
-          <Toolbar className={classes.toolbar}>
-            <Typography
-              variant="h6"
-              color="inherit"
-              noWrap
-              className={classes.toolbarTitle}
-            >
-              MovieDB
-            </Typography>
-            <nav>
-              <Link
-                variant="button"
-                color="textPrimary"
-                href="#"
-                className={classes.link}
-              >
-                Yes
-              </Link>
-              <Link
-                variant="button"
-                color="textPrimary"
-                href="#"
-                className={classes.link}
-              >
-                Hello
-              </Link>
-              <Link
-                variant="button"
-                color="textPrimary"
-                href="#"
-                className={classes.link}
-              >
-                Mate
-              </Link>
-            </nav>
-            <Button
-              href="#"
-              color="primary"
-              variant="outlined"
-              className={classes.link}
-            >
-              Login
-            </Button>
-          </Toolbar>
-        </AppBar>
+        <Header />
         {/* Hero unit */}
         <Container
           maxWidth="sm"
@@ -230,123 +103,24 @@ export default function Layout() {
           className={classes.heroContent}
         >
           <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="textPrimary"
-            gutterBottom
-          >
-            Pricing
-          </Typography>
-          <Typography
             variant="h5"
             align="center"
             color="textSecondary"
             component="p"
           >
-            Quickly build an effective pricing table for your potential
-            customers with this layout. It&apos;s built with default Material-UI
-            components with little customization.
+            Top rated
           </Typography>
         </Container>
         {/* End hero unit */}
         <Container maxWidth="md" component="main">
-          <Grid container spacing={5} alignItems="flex-end">
-          <Grid item key={'2'} xs={12} sm={6} md={4}>
-            <Card className={classes.root}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  className={classes.media}
-                  src='placeholder.jpg'
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Lizard
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Share
-                </Button>
-                <Button size="small" color="primary">
-                  Learn More
-                </Button>
-              </CardActions>
-            </Card>
-            </Grid>
-
-            <Grid item key={'3'} xs={12} sm={6} md={4}>
-            <Card className={classes.root}>
-              <CardActionArea>
-                <CardMedia
-                component="img"
-                  className={classes.media}
-                  image="placeholder.jpg"
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Lizard
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Share
-                </Button>
-                <Button size="small" color="primary">
-                  Learn More
-                </Button>
-              </CardActions>
-            </Card>
-            </Grid>
-          </Grid>
+          <Carousel autoPlay = {false} indicators={false} navButtonsAlwaysVisible = {true}>
+            {items.map((item, i) => (
+              <Item key={i} item={item} />
+            ))}
+          </Carousel>
         </Container>
         {/* Footer */}
-        <Container maxWidth="md" component="footer" className={classes.footer}>
-          <Grid container spacing={4} justify="space-evenly">
-            {footers.map((footer) => (
-              <Grid item xs={6} sm={3} key={footer.title}>
-                <Typography variant="h6" color="textPrimary" gutterBottom>
-                  {footer.title}
-                </Typography>
-                <ul>
-                  {footer.description.map((item) => (
-                    <li key={item}>
-                      <Link href="#" variant="subtitle1" color="textSecondary">
-                        {item}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </Grid>
-            ))}
-          </Grid>
-          <Box mt={5}>
-            <Copyright />
-          </Box>
-        </Container>
+        <Footer />
         {/* End footer */}
       </ThemeProvider>
     </>
